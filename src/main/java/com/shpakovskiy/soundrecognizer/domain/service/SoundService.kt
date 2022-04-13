c735c3eb009545c0c11b7143529e80d712e4b679
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2022-04-13 20:38:11 +0300

    Add temporary implementation of audiofile saving

--- src/main/java/com/shpakovskiy/soundrecognizer/domain/service/SoundService.kt
+++ src/main/java/com/shpakovskiy/soundrecognizer/domain/service/SoundService.kt
@@ -3,13 +3,30 @@ package com.shpakovskiy.soundrecognizer.domain.service
 import com.shpakovskiy.soundrecognizer.SoundRecognizerService
 import com.shpakovskiy.soundrecognizer.SoundServiceGrpc
 import io.grpc.stub.StreamObserver
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class SoundService : SoundServiceGrpc.SoundServiceImplBase() {
     override fun sendSound(
         request: SoundRecognizerService.Sound?,
         responseObserver: StreamObserver<SoundRecognizerService.EmptyResponse>?
     ) {
+        request?.let {
+            Files.write(
+                Paths.get("recordings/recognitionRequests/${it.fileName}"),
+                it.soundValues.toByteArray()
+            )
+        }
+
         println("Received file: ${request?.soundValues?.size()} bytes")
+
+        val response = SoundRecognizerService.EmptyResponse
+            .newBuilder()
+            .setReceivedSoundLength(request?.soundValues?.size() ?: -1)
+            .build()
+
+        responseObserver?.onNext(response)
+        responseObserver?.onCompleted()
         super.sendSound(request, responseObserver)
     }
 }
