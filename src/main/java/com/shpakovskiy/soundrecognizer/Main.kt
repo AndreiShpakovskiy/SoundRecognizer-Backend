1a7f97532c1719b27c0d9c4ba24f5fca0c6068d5
Andrei Shpakovskiy <shpakovskiyandrei@gmail.com>
2023-05-12 15:04:40 +0300

    Try Ktor

--- src/main/java/com/shpakovskiy/soundrecognizer/Main.kt
+++ src/main/java/com/shpakovskiy/soundrecognizer/Main.kt
@@ -1,11 +1,21 @@
 package com.shpakovskiy.soundrecognizer
 
-import com.shpakovskiy.soundrecognizer.data.service.SoundService
-import io.grpc.ServerBuilder
-import kotlinx.coroutines.runBlocking
-import java.util.concurrent.Executors
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
+    embeddedServer(Netty, port = 8099) {
+        routing {
+            get("/") {
+                call.respond("Want to home")
+            }
+        }
+    }.start(wait = true)
+
+    /*
     runBlocking {
         ServerBuilder.forPort(8099)
             .executor(Executors.newFixedThreadPool(10))
@@ -20,4 +30,5 @@ fun main() {
                 awaitTermination()
             }
     }
+     */
 }
